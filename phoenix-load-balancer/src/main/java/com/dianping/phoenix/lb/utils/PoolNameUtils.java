7dbf4879eb6c47b5fe17031fce4bc98b44f87262
Leo Liang <liangjinhua@gmail.com>
2013-12-11 15:57:29 +0800

    pool name bugfix

---
+++ phoenix-load-balancer/src/main/java/com/dianping/phoenix/lb/utils/PoolNameUtils.java
@@ -0,0 +1,22 @@
+/**
+ * Project: phoenix-load-balancer
+ * 
+ * File Created at Dec 11, 2013
+ * 
+ */
+package com.dianping.phoenix.lb.utils;
+
+/**
+ * @author Leo Liang
+ *
+ */
+public class PoolNameUtils {
+    public static String getPoolNamePrefix(String poolName) {
+        int prefixPos = poolName.indexOf("@");
+        if (prefixPos >= 0) {
+            return poolName.substring(0, prefixPos);
+        } else {
+            return poolName;
+        }
+    }
+}
