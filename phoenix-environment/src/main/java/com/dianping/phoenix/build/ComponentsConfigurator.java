fce674de9f43fd78d7a660c4445117d6d69976fe
marsqing <qing.gu@dianping.com>
2013-12-20 13:47:28 +0800

    change PhoenixEnvironmentFilter from Filter to PhoenixFilter

--- phoenix-environment/src/main/java/com/dianping/phoenix/build/ComponentsConfigurator.java
+++ phoenix-environment/src/main/java/com/dianping/phoenix/build/ComponentsConfigurator.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.unidal.lookup.configuration.AbstractResourceConfigurator;
 import org.unidal.lookup.configuration.Component;
 
+import com.dianping.phoenix.environment.PhoenixEnvironmentFilter;
 import com.dianping.phoenix.servlet.PhoenixFilterHandler;
 import com.dianping.phoenix.session.RequestEventDelegate;
 import com.dianping.phoenix.session.RequestIdHandler;
@@ -15,6 +16,7 @@ public class ComponentsConfigurator extends AbstractResourceConfigurator {
 	public List<Component> defineComponents() {
 		List<Component> all = new ArrayList<Component>();
 
+		all.add(C(PhoenixFilterHandler.class, PhoenixEnvironmentFilter.ID, PhoenixEnvironmentFilter.class));
 		all.add(C(PhoenixFilterHandler.class, RequestIdHandler.ID, RequestIdHandler.class));
 		all.add(C(RequestEventDelegate.class).is(PER_LOOKUP));
 
--- phoenix-environment/src/main/java/com/dianping/phoenix/environment/PhoenixEnvironmentFilter.java
+++ phoenix-environment/src/main/java/com/dianping/phoenix/environment/PhoenixEnvironmentFilter.java
@@ -3,26 +3,28 @@ package com.dianping.phoenix.environment;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.plexus.logging.Logger;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 import org.unidal.lookup.logger.LoggerFactory;
 import org.unidal.net.Networks;
 
-public class PhoenixEnvironmentFilter implements Filter {
+import com.dianping.phoenix.servlet.PhoenixFilterContext;
+import com.dianping.phoenix.servlet.PhoenixFilterHandler;
+
+public class PhoenixEnvironmentFilter implements PhoenixFilterHandler, Initializable {
 	private final Logger m_logger = LoggerFactory.getLogger(PhoenixEnvironmentFilter.class);
 
 	public static final String PHOENIX_ID_COOKIE_NAME = "PHOENIX_ID";
 
 	private final static char[] HEX_DIGITS = "0123456789abcdef".toCharArray();
+
+	public static final String ID = "phoenix-env";
 
 	private String m_ip;
 
@@ -30,12 +32,80 @@ public class PhoenixEnvironmentFilter implements Filter {
 
 	private AtomicInteger m_phoenix_id_index = new AtomicInteger(0);
 
-	@Override
-	public void destroy() {
+	private String bytesToHex(byte[] bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		int v;
+		for (int j = 0; j < bytes.length; j++) {
+			v = bytes[j] & 0xFF;
+			hexChars[j * 2] = HEX_DIGITS[v >>> 4];
+			hexChars[j * 2 + 1] = HEX_DIGITS[v & 0x0F];
+		}
+		return new String(hexChars);
 	}
 
-	private void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException,
-	      ServletException {
+	private String generatePhoenixId() {
+		long ts = System.currentTimeMillis();
+		int seq = m_phoenix_id_index.incrementAndGet();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(m_ip);
+		sb.append("-");
+		sb.append(Long.toHexString(ts));
+		sb.append("-");
+		sb.append(Integer.toHexString(seq));
+
+		return sb.toString();
+	}
+
+	private String generateRequestId() {
+		long ts = System.currentTimeMillis();
+		int seq = m_req_index.incrementAndGet();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(m_ip);
+		sb.append("-");
+		sb.append(Long.toHexString(ts));
+		sb.append("-");
+		sb.append(Integer.toHexString(seq));
+
+		return sb.toString();
+	}
+
+	private String getCookie(HttpServletRequest req, String name) {
+		Cookie[] cookies = req.getCookies();
+
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals(name)) {
+					return cookie.getValue();
+				}
+			}
+		}
+
+		return null;
+	}
+
+	private String getOrCreatePhoenixId(HttpServletRequest req, HttpServletResponse res) {
+		String phoenixId = getCookie(req, PHOENIX_ID_COOKIE_NAME);
+
+		if (phoenixId == null) {
+			phoenixId = generatePhoenixId();
+			setCookie(res, PHOENIX_ID_COOKIE_NAME, phoenixId);
+		}
+
+		return phoenixId;
+	}
+
+	@Override
+	public int getOrder() {
+		return 0;
+	}
+
+	@Override
+	public void handle(PhoenixFilterContext ctx) throws IOException, ServletException {
+		HttpServletRequest req = ctx.getHttpServletRequest();
+		HttpServletResponse res = ctx.getHttpServletResponse();
+
 		try {
 			// 从request中或去id
 			String requestId = req.getHeader(PhoenixContext.MOBILE_REQUEST_ID);
@@ -68,7 +138,7 @@ public class PhoenixEnvironmentFilter implements Filter {
 		}
 
 		try {
-			chain.doFilter(req, res);
+			ctx.doFilter();
 		} finally {
 			// 清除ThreadLocal
 			PhoenixContext.getInstance().clear();
@@ -76,81 +146,11 @@ public class PhoenixEnvironmentFilter implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-	      ServletException {
-		doFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
-	}
-
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
+	public void initialize() throws InitializationException {
 		m_ip = bytesToHex(Networks.forIp().getLocalAddress());
-	}
-
-	private String generateRequestId() {
-		long ts = System.currentTimeMillis();
-		int seq = m_req_index.incrementAndGet();
-
-		StringBuilder sb = new StringBuilder();
-		sb.append(m_ip);
-		sb.append("-");
-		sb.append(Long.toHexString(ts));
-		sb.append("-");
-		sb.append(Integer.toHexString(seq));
-
-		return sb.toString();
-	}
-
-	private String getOrCreatePhoenixId(HttpServletRequest req, HttpServletResponse res) {
-		String phoenixId = getCookie(req, PHOENIX_ID_COOKIE_NAME);
-
-		if (phoenixId == null) {
-			phoenixId = generatePhoenixId();
-			setCookie(res, PHOENIX_ID_COOKIE_NAME, phoenixId);
-		}
-
-		return phoenixId;
-	}
-
-	private String generatePhoenixId() {
-		long ts = System.currentTimeMillis();
-		int seq = m_phoenix_id_index.incrementAndGet();
-
-		StringBuilder sb = new StringBuilder();
-		sb.append(m_ip);
-		sb.append("-");
-		sb.append(Long.toHexString(ts));
-		sb.append("-");
-		sb.append(Integer.toHexString(seq));
-
-		return sb.toString();
 	}
 
 	private void setCookie(HttpServletResponse res, String cookieName, String cookieValue) {
 		res.addCookie(new Cookie(cookieName, cookieValue));
-	}
-
-	private String getCookie(HttpServletRequest req, String name) {
-		Cookie[] cookies = req.getCookies();
-
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				if (cookie.getName().equals(name)) {
-					return cookie.getValue();
-				}
-			}
-		}
-
-		return null;
-	}
-
-	private String bytesToHex(byte[] bytes) {
-		char[] hexChars = new char[bytes.length * 2];
-		int v;
-		for (int j = 0; j < bytes.length; j++) {
-			v = bytes[j] & 0xFF;
-			hexChars[j * 2] = HEX_DIGITS[v >>> 4];
-			hexChars[j * 2 + 1] = HEX_DIGITS[v & 0x0F];
-		}
-		return new String(hexChars);
 	}
 }
