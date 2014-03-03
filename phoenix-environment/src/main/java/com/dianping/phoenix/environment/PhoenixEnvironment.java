991b88821141c3bc9dbfaaefdf0aa78d28de8a63
marsqing <qing.gu@dianping.com>
2014-03-03 17:40:17 +0800

    add refer request id

--- phoenix-environment/src/main/java/com/dianping/phoenix/environment/PhoenixEnvironment.java
+++ phoenix-environment/src/main/java/com/dianping/phoenix/environment/PhoenixEnvironment.java
@@ -14,25 +14,41 @@ public class PhoenixEnvironment {
 
 	private String m_guid;
 
+	private String m_referRequestId;
+
 	public PhoenixEnvironment(String requestId, String guid) {
 		m_requestId = requestId;
 		m_guid = guid;
 	}
 
-	public String getRequestId() {
-		return m_requestId;
-	}
-
-	public void setRequestId(String requestId) {
+	public PhoenixEnvironment(String requestId, String guid, String referRequestId) {
 		m_requestId = requestId;
+		m_guid = guid;
+		m_referRequestId = referRequestId;
 	}
 
 	public String getGuid() {
 		return m_guid;
 	}
 
+	public String getReferRequestId() {
+		return m_referRequestId;
+	}
+
+	public String getRequestId() {
+		return m_requestId;
+	}
+
 	public void setGuid(String guid) {
 		m_guid = guid;
+	}
+
+	public void setReferRequestId(String referRequestId) {
+		m_referRequestId = referRequestId;
+	}
+
+	public void setRequestId(String requestId) {
+		m_requestId = requestId;
 	}
 
 }
