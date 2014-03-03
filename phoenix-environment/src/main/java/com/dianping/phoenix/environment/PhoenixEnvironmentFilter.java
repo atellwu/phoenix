9147cd7ffe359b99dfc4cf8f56c9f962963c341d
marsqing <qing.gu@dianping.com>
2014-03-03 10:52:38 +0800

    remove unnecessary comment

--- phoenix-environment/src/main/java/com/dianping/phoenix/environment/PhoenixEnvironmentFilter.java
+++ phoenix-environment/src/main/java/com/dianping/phoenix/environment/PhoenixEnvironmentFilter.java
@@ -117,27 +117,21 @@ public class PhoenixEnvironmentFilter implements PhoenixFilterHandler, Initializ
 
 		String requestId = null;
 		try {
-			// 从request中或去id
 			requestId = req.getHeader(PhoenixContext.MOBILE_REQUEST_ID);
 			String referRequestId = null;
 
-			if (requestId != null) {// 如果存在requestId，则说明是移动api的web端
+			if (requestId != null) {
 				referRequestId = req.getHeader(PhoenixContext.MOBILE_REFER_REQUEST_ID);
-			} else {// 普通web端 TODO 待第二期实现
-				// requestId不存在，则生成
-				// referRequestId，异步通过pigeon去session服务器获取
-				// 判断cookie中的guid是否存在，不存在则生成
-				// 将所有id放入request属性，供页头使用
-				// request.setAttribute(PhoenixEnvironment.ENV, new PhoenixEnvironment());
+			} else {
 				requestId = generateRequestId();
 			}
 
 			String phoenixId = getOrCreatePhoenixId(req, res);
 			PhoenixContext.getInstance().setGuid(phoenixId);
 
+			// for decorator service
 			req.setAttribute(PhoenixEnvironment.ENV, new PhoenixEnvironment(requestId, phoenixId));
 
-			// 将id放入ThreadLocal
 			if (requestId != null) {
 				PhoenixContext.getInstance().setRequestId(requestId);
 			}
@@ -155,7 +149,6 @@ public class PhoenixEnvironmentFilter implements PhoenixFilterHandler, Initializ
 			if (requestId != null) {
 				res.setHeader(REQUEST_HEADER_NAME, requestId);
 			}
-			// 清除ThreadLocal
 			PhoenixContext.getInstance().clear();
 		}
 	}
