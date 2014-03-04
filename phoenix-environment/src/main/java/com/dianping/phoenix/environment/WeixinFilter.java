d1fa6a802be1d7304d6ab4efbbc7bf32aef7fedc
marsqing <qing.gu@dianping.com>
2014-03-04 11:02:25 +0800

    read parameter name from config and bump to 0.3.1

--- phoenix-environment/src/main/java/com/dianping/phoenix/environment/WeixinFilter.java
+++ phoenix-environment/src/main/java/com/dianping/phoenix/environment/WeixinFilter.java
@@ -9,29 +9,42 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.apache.log4j.Logger;
+
 public class WeixinFilter implements Filter {
+
+	private static final Logger m_logger = Logger.getLogger(WeixinFilter.class);
 
 	private static final String REFER_REQUEST_ID = "rid";
 
-	@Override
-   public void init(FilterConfig filterConfig) throws ServletException {
-   }
+	private static final String PARAMETER_NAME = "parameter-name";
+
+	private String m_parameterName = REFER_REQUEST_ID;
 
 	@Override
-   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
-         ServletException {
-		
-		String referRequestId = req.getParameter(REFER_REQUEST_ID);
-		
-		if(referRequestId != null) {
+	public void init(FilterConfig filterConfig) throws ServletException {
+		String configuredName = filterConfig.getInitParameter(PARAMETER_NAME);
+		if (configuredName != null && configuredName.length() > 0) {
+			m_parameterName = configuredName;
+		}
+		m_logger.info(String.format("Will use request parameter (%s) as refer request id from weixin", m_parameterName));
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
+	      ServletException {
+
+		String referRequestId = req.getParameter(m_parameterName);
+
+		if (referRequestId != null) {
 			PhoenixContext.getInstance().setReferRequestId(referRequestId);
 		}
-		
+
 		chain.doFilter(req, res);
-   }
+	}
 
 	@Override
-   public void destroy() {
-   }
+	public void destroy() {
+	}
 
 }
