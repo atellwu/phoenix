4131f20779a53b7bfcf001f298ffdd328a7dad39
marsqing <qing.gu@dianping.com>
2014-04-09 14:41:09 +0800

    parallel it

--- phoenix-dev-agent/src/test/resources/update_all_alphas.sh
+++ phoenix-dev-agent/src/test/resources/update_all_alphas.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-IP_BASE="192.168.214."
+IP_BASE="10.1.77."
 PORT=58422
 USER="root"
 PWD="12qwaszx"
-IP_START=141
-IP_END=170
+IP_START=193
+IP_END=193
 
 if [ $# != 1 ] ; then 
     echo "USAGE: $0 war_path" 
@@ -15,5 +15,9 @@ fi
 
 for (( i=$IP_START; i<=$IP_END; i++ ))
 do
-    ./deploy.sh $IP_BASE$i $PORT $USER $PWD $1
+    ./deploy.sh $IP_BASE$i $PORT $USER $PWD $1 &
 done
+
+wait
+
+echo "ALL DONE"
